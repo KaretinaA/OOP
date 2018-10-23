a078d4f462cb765107655cef85d30f14ed9480e8
KaretinaA <AleksAnna-92@mail.ru>
2018-10-23 16:35:51 +0300

    8

---
+++ src/test/java/DelenieTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Anna Karetina on 21.10.2018.
+ */
+public class DelenieTest {
+    @Test
+    public void getOperator() throws Exception {
+        Delenie oper=new Delenie();
+        String actual=oper.getOperator();
+        String expected="/";
+        assertEquals(expected,actual);
+        System.out.println("Знак деления корректен");
+    }
+
+    @Test
+    public void calculateResult() throws Exception {
+        Delenie summat=new Delenie();
+        double actual = summat.calculateResult();
+        double expected = 2;
+        assertThat(expected,equalTo(actual));
+        System.out.println("деление отрабатывает корректно");
+
+    }
+
+}
